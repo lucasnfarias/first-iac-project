c8a4dc421e288f299479f333cde5cdcd0d8eaa28
Lucas Farias <lucas.farias@rtbhouse.com>
2024-11-19 12:03:02 -0300

    create cloudfront module

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
 module "s3" {
   source         = "./modules/s3"
-  s3_bucket_name = "rocketseat-iac"
+  s3_bucket_name = "rocketseat-iac-lfarias"
+}
+
+module "cloudfront" {
+  source = "./modules/cloudfront"
+
+  origin_id          = module.s3.bucket_id
+  bucket_domain_name = module.s3.bucket_domain_name
+
+  depends_on = [
+    module.s3
+  ]
 }
---
+++ modules/cloudfront/variables.tf
@@ -0,0 +1,9 @@
+variable "origin_id" {
+  type        = string
+  description = "S3 id"
+}
+
+variable "bucket_domain_name" {
+  type        = string
+  description = "S3 domain"
+}
